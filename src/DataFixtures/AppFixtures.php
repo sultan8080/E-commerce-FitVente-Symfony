b721b14d883fd9abfeb4f59e2e8fa7cc982753e4
Moti-Sultan <motisultannur@gmail.com>
2025-03-16 16:25:34 +0100

    Data Fixture category created

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,11 +2,12 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Category;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -20,7 +21,6 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
-
         // Create Users
         $users = [];
         for ($i = 0; $i < 2; $i++) {
@@ -34,7 +34,7 @@ class AppFixtures extends Fixture
             $manager->persist($user);
             $users[] = $user;
         }
-        
+
         for ($i = 0; $i < 2; $i++) {
             $admin = new User();
             $admin->setEmail($faker->email);
@@ -45,7 +45,17 @@ class AppFixtures extends Fixture
             $admin->setCreatedAt(new \DateTimeImmutable());
             $manager->persist($admin);
             $users[] = $admin;
-        }    
+        }
+         // Create Categories
+         $categories = [];
+         for ($i = 0; $i < 5; $i++) {
+             $category = new Category();
+             $category->setName($faker->word);
+             $category->setDescription($faker->sentence);
+             $manager->persist($category);
+             $categories[] = $category;
+         }
+         
         $manager->flush();
     }
 }
