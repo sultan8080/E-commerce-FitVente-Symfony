105098dac46f407b37b20f3a95d624412cbed0d4
Moti-Sultan <motisultannur@gmail.com>
2025-03-16 16:15:01 +0100

    Data Fixture users data created

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,50 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
 
+        // Create Users
+        $users = [];
+        for ($i = 0; $i < 2; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setPassword($this->passwordHasher->hashPassword($user, "password123"));
+            $user->setRoles(["ROLE_USER"]);
+            $user->setFirstName($faker->firstName);
+            $user->setLastName($faker->lastName);
+            $user->setCreatedAt(new \DateTimeImmutable());
+            $manager->persist($user);
+            $users[] = $user;
+        }
+        
+        for ($i = 0; $i < 2; $i++) {
+            $admin = new User();
+            $admin->setEmail($faker->email);
+            $admin->setPassword($this->passwordHasher->hashPassword($admin, "admin123"));
+            $admin->setRoles(["ROLE_ADMIN"]);
+            $admin->setFirstName($faker->firstName);
+            $admin->setLastName($faker->lastName);
+            $admin->setCreatedAt(new \DateTimeImmutable());
+            $manager->persist($admin);
+            $users[] = $admin;
+        }    
         $manager->flush();
     }
 }
